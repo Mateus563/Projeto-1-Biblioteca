d1fff43ee10c5fabcbf9f67b29ce30296a9a926c
Mateus Morás <mateus.moras@universo.univates.br>
2026-05-19 20:02:46 -0300

    Create tables for livro and leitor

--- creates.sql
+++ creates.sql
@@ -1,1 +1,14 @@
+create table livro (
+	id SERIAL PRIMARY KEY,
+	titulo VARCHAR(100),
+	autor VARCHAR(100),
+	genero VARCHAR(50),
+	anopublicacao DATE
+);
 
+create table leitor(
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(100),
+	cpf VARCHAR(11),
+	telefone VARCHAR(11)
+  );
